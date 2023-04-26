817760957b8c8f8af19aa2b6cf94ea0f6a012466
Nick Talati <nicholas.talati@duke.edu>
2023-04-26 11:43:47 -0400

    made a ton of plots

--- csv_creator.py
+++ csv_creator.py
@@ -3,10 +3,36 @@ import re
 import tensorflow as tf
 from complexity_model import get_complexity_scores
 
+
+def material_count(fen):
+    # Standard material values for each piece type
+    piece_values = {'P': 1, 'N': 3, 'B': 3, 'R': 5, 'Q': 9,
+                    'p': 1, 'n': 3, 'b': 3, 'r': 5, 'q': 9}
+
+    # Extract the piece placement string from the FEN
+    piece_placement = fen.split()[0]
+
+    # Initialize material counts for both sides
+    white_material = 0
+    black_material = 0
+
+    # Iterate through the piece placement string
+    for char in piece_placement:
+        if char in piece_values:
+            # Add the material value of the piece to the respective side's total count
+            if char.isupper():
+                white_material += piece_values[char]
+            else:
+                black_material += piece_values[char]
+
+    return white_material, black_material
+
+
 def process_csv(input_file, output_file, model):
     with open(input_file, 'r') as infile, open(output_file, 'w', newline='') as outfile:
         reader = csv.DictReader(infile)
-        fieldnames = ['white_elo', 'black_elo', 'white_time_left', 'black_time_left', 'expected_loss', 'blunder_chance', 'move_time']
+        fieldnames = ['white_elo', 'black_elo', 'white_time_left', 'black_time_left',
+                      'white_material', 'black_material', 'expected_loss', 'blunder_chance', 'move_time']
         writer = csv.DictWriter(outfile, fieldnames=fieldnames)
         writer.writeheader()
         game = -1
@@ -27,6 +53,7 @@ def process_csv(input_file, output_file, model):
 
             ply += 1
             fen = fens[ply]
+            white_material, black_material = material_count(fen)
             fens.append(row['FEN'])
             wb = fen.split(' ')[1]
         
@@ -38,7 +65,7 @@ def process_csv(input_file, output_file, model):
                 black_time_left = move_times[ply]
                 white_time_left = move_times[ply + 1]
 
-            complexity_scores = get_complexity_scores(fens[ply], (white_elo + black_elo) / 2, model)
+            complexity_scores = get_complexity_scores(fen, (white_elo + black_elo) / 2, model)
             expected_loss = complexity_scores['cp_loss']
             blunder_chance = complexity_scores['blunder_chance']
 
@@ -50,6 +77,8 @@ def process_csv(input_file, output_file, model):
                 'black_elo': black_elo,
                 'white_time_left': round(white_time_left, 2),
                 'black_time_left': round(black_time_left, 2),
+                'white_material': white_material,
+                'black_material': black_material,
                 'expected_loss': round(expected_loss, 2),
                 'blunder_chance': round(blunder_chance, 5),
                 'move_time': round(move_time, 2)
@@ -58,8 +87,8 @@ def process_csv(input_file, output_file, model):
             writer.writerow(output_row)
 
 input_csv = 'new_games_cond.csv'
-output_csv = 'training_data.csv'
+output_csv = 'training_data2.csv'
 # input_csv = 'input.csv'
 # output_csv = 'output.csv'
-model = tf.keras.models.load_model("complexity_model.h5")
+model = tf.keras.models.load_model("complexity_model/complexity_model.h5")
 process_csv(input_csv, output_csv, model)
--- time_models.py
+++ time_models.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import matplotlib.lines as mlines
+import matplotlib.cm as cm
+import matplotlib.colors as mcolors
 import pandas as pd
 import tensorflow as tf
 import tensorflow_probability as tfp
@@ -69,13 +71,14 @@ def get_bayesian_nn_time_model(df, epochs=5, training_split=0.8):
     return bayesian_nn_time_model
 
 
-def plot_actual_vs_predicted(model, df):
+def plot_actual_vs_predicted(model, df, means, stds):
     df_copy = df.copy()
-    xs = df[df.columns[:-1]]
-    ys = df['move_time']
-    df_predicted = model.predict(xs)
+    xs = (df[df.columns[:-1]] * stds) + means
+    df_predicted = model.predict(df[df.columns[:-1]])
     df_copy["predicted_time"] = df_predicted
-    df_copy = df_copy.loc[(df_copy['move_time'] < 45) & (df_copy['predicted_time'] < 45)]
+    df_copy[df_copy.columns[:-2]] = xs
+    valid_times = (df_copy['move_time'] < 45) & (df_copy['predicted_time'] < 45)
+    df_copy = df_copy.loc[valid_times]
     num_columns = len(df_copy.columns) - 2
 
     # Calculate the number of rows and columns for the subplots
@@ -114,20 +117,113 @@ def plot_actual_vs_predicted(model, df):
     plt.show()
 
 
+def plot_side_by_side(model, df, means, stds):
+    df_copy = df.copy()
+    xs = (df[df.columns[:-1]] * stds) + means
+    df_predicted = model.predict(df[df.columns[:-1]])
+    df_copy["predicted_time"] = df_predicted
+    df_copy[df_copy.columns[:-2]] = xs
+    valid_times = (df_copy['move_time'] < 45) & (df_copy['predicted_time'] < 45)
+    df_copy = df_copy.loc[valid_times]
+    valid_times = (df_copy['move_time'] < 45) & (df_copy['predicted_time'] < 45)
+    df_copy = df_copy.loc[valid_times]
+
+    for idx, column in enumerate(df_copy.columns[:-2]):
+        fig, axs = plt.subplots(1, 2, figsize=(16, 4))
+
+        # Plot the actual time
+        axs[0].scatter(df_copy[column], df_copy['move_time'], color='blue', alpha=0.003)
+        axs[0].set_title(f"{' '.join(column.split('_'))} vs. actual move time")
+        axs[0].set_xlabel(' '.join(column.split('_')))
+        axs[0].set_ylabel("actual move time")
+
+        # Plot the predicted time
+        axs[1].scatter(df_copy[column], df_copy['predicted_time'], color='red', alpha=0.003)
+        axs[1].set_title(f"{' '.join(column.split('_'))} vs. predicted move time")
+        axs[1].set_xlabel(' '.join(column.split('_')))
+        axs[1].set_ylabel("predicted move time")
+
+        plt.tight_layout()
+        plt.show()
+
+
+def plot_with_color_scale(model, df, means, stds):
+    df_copy = df.copy()
+    xs = (df[df.columns[:-1]] * stds) + means
+    df_predicted = model.predict(df[df.columns[:-1]])
+    df_copy["predicted_time"] = df_predicted
+    df_copy[df_copy.columns[:-2]] = xs
+    valid_times = (df_copy['move_time'] < 45) & (df_copy['predicted_time'] < 45)
+    df_copy = df_copy.loc[valid_times]
+    df_copy[['white_material', 'black_material']] += np.random.normal(0, 0.5, size=(len(df_copy), 2))
+
+    # Define the columns for the joint distributions
+    joint_columns = [['white_elo', 'black_elo'],
+                     ['white_time_left', 'black_time_left'],
+                     ['white_material', 'black_material']]
+
+    # Define the color maps
+    cmap = cm.get_cmap('coolwarm')
+    norm = mcolors.Normalize(vmin=0, vmax=5)
+
+    # Iterate through the joint columns
+    for columns in joint_columns:
+        fig, axs = plt.subplots(1, 3, figsize=(16, 4), gridspec_kw={'width_ratios': [1, 1, 0.05]})
+
+        # Plot the actual time
+        colors = cmap(norm(df_copy['move_time']))
+        colors[:, 3] = 0.005
+        scatter = axs[0].scatter(df_copy[columns[0]], df_copy[columns[1]], c=colors)
+        axs[0].set_title(f"{' '.join(columns[0].split('_'))} vs {' '.join(columns[1].split('_'))} (actual move time)")
+        axs[0].set_xlabel(' '.join(columns[0].split('_')))
+        axs[0].set_ylabel(' '.join(columns[1].split('_')))
+
+        # Plot the predicted time
+        colors = cmap(norm(df_copy['predicted_time']))
+        colors[:, 3] = 0.005
+        scatter = axs[1].scatter(df_copy[columns[0]], df_copy[columns[1]], c=colors)
+        axs[1].set_title(f"{' '.join(columns[0].split('_'))} vs {' '.join(columns[1].split('_'))} (predicted move time)")
+        axs[1].set_xlabel(' '.join(columns[0].split('_')))
+        axs[1].set_ylabel(' '.join(columns[1].split('_')))
+
+        # Remove the ticks and spines of the empty axis
+        axs[2].set_xticks([])
+        axs[2].set_yticks([])
+        for spine in axs[2].spines.values():
+            spine.set_visible(False)
+
+        # Add a colorbar to the figure
+        sm = cm.ScalarMappable(cmap=cmap, norm=norm)
+        sm.set_array([])
+        fig.colorbar(sm, cax=axs[2], label="move time")
+
+        plt.tight_layout()
+        plt.show()
+
+
 if __name__ == '__main__':
-    csv_path = 'training_data.csv'
+    csv_path = 'training_data2.csv'
     df = pd.read_csv(csv_path)
+    valid_w_elos = (df['white_elo'] <= 1600) & (df['white_elo'] >= 1400)
+    valid_b_elos = (df['black_elo'] <= 1600) & (df['black_elo'] >= 1400)
+    df = df.loc[valid_w_elos & valid_b_elos]
+
     means = df[df.columns[:-1]].mean()
     stds = df[df.columns[:-1]].std()
     df[df.columns[:-1]] = (df[df.columns[:-1]] - means) / stds
     df['move_time'] += 0.05
     print(df['move_time'].min())
 
-    # nn_time_model = get_nn_time_model(df, epochs=5, training_split=0.8)
+    # nn_time_model = get_nn_time_model(df, epochs=10, training_split=0.8)
     # plot_actual_vs_predicted(nn_time_model, df)
 
-    bayesian_nn_time_model = get_bayesian_nn_time_model(df, epochs=2, training_split=0.8)
-    plot_actual_vs_predicted(bayesian_nn_time_model, df)
+    bayesian_nn_time_model = get_bayesian_nn_time_model(df, epochs=10, training_split=0.8)
+    tf.keras.models.save_model(bayesian_nn_time_model, 'bayesian_nn_time_model.h5')
+
+    # plot_side_by_side(bayesian_nn_time_model, df, means, stds)
+    # plot_with_color_scale(bayesian_nn_time_model, df, means, stds)
+
+    # plot_actual_vs_predicted(bayesian_nn_time_model, df, means, stds)
 
 
 
